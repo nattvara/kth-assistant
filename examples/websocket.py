06ffe28e65305d2d199fe3b828948e1aa9b79b51
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-04 18:42:52 +0100

    add docstring to example clarifying what the example shows

--- examples/websocket.py
+++ examples/websocket.py
@@ -1,3 +1,15 @@
+"""
+The purpose of this example is how to create a prompt handle, and very simply
+demonstrate how the websocket connection is setup. This example shows
+a worker sending a message that will be received by a requester.
+
+NOTE: this example needs a redis and postgres server to be running
+
+$ docker run --name some-postgres -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres
+$ docker run -p 6379:6379 -it redis/redis-stack:latest
+
+"""
+
 import asyncio
 import websockets
 
