8db806ec9b79b8acef3ee3f7246341a1a80328fe
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-10 21:11:19 +0100

    wrap the app component in an AppShell component

--- gui/pages/_app.tsx
+++ gui/pages/_app.tsx
@@ -1,20 +1,50 @@
+import { Group, MantineProvider, Skeleton } from "@mantine/core";
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import "@mantine/core/styles.css";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+
 import { theme } from "../theme";
 
 export default function App({ Component, pageProps }: any) {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <MantineProvider theme={theme}>
       <Head>
-        <title>Mantine Template</title>
+        <title>KTH Assistant</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
-      <Component {...pageProps} />
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            KTH Assistant
+          </Group>
+        </AppShell.Header>
+
+        <AppShell.Navbar p="md">
+          <Skeleton h={28} mt="sm" animate={false} />
+          <Skeleton h={28} mt="sm" animate={false} />
+          <Skeleton h={28} mt="sm" animate={false} />
+        </AppShell.Navbar>
+
+        <AppShell.Main>
+          <Component {...pageProps} />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
