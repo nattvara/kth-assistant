c8424b6fbe11f4b80ed10abc96e7adfbf641c9ab
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-14 21:11:26 +0100

    fix issue where migratehistory table prevented database from migrating after tables being dropped

--- tests/db/migrations/test_migrations.py
+++ tests/db/migrations/test_migrations.py
@@ -9,11 +9,12 @@ from tests.assertions import assert_table_count
 
 def test_all_migrations_can_be_applied():
     db.drop_tables(all_models)
+    db.execute_sql("DROP TABLE IF EXISTS `migratehistory`")
 
     router = Router(db, migrate_dir=MIGRATIONS_DIR, logger=log())
     router.run()
 
-    assert_table_count(2)
+    assert_table_count(len(all_models) + 1)  # one extra table due to the migratehistory table
 
 
 def test_all_migrations_can_be_rolled_back():
