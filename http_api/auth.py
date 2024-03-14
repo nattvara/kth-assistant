8528bdd08a608362fde5d77d2d508c4a3fbb7585
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-14 22:46:00 +0100

    fix issue where '...is True' doesn't create the correct query

--- http_api/auth.py
+++ http_api/auth.py
@@ -7,7 +7,7 @@ api_key_header = APIKeyHeader(name='X-Session-ID', auto_error=False)
 
 async def get_current_session(session_id: str = Security(api_key_header)) -> Session:
     try:
-        session = Session.get(Session.public_id == session_id, Session.is_valid is True)
+        session = Session.get(Session.public_id == session_id, Session.is_valid == True)  # noqa
         return session
     except Session.DoesNotExist:
         raise HTTPException(
