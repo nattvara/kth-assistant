dbe49b738f22cb907d707cd5140b8cab9ff4883d
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-16 16:36:40 +0100

    add Chat model

---
+++ db/models/chat.py
@@ -0,0 +1,30 @@
+import warnings
+import uuid
+
+import peewee
+
+from . import BaseModel, Course, Session
+
+# Suppress specific DeprecationWarning about db_table, this is needed for migrations to work
+warnings.filterwarnings(
+    "ignore",
+    message='"db_table" has been deprecated in favor of "table_name" for Models.',
+    category=DeprecationWarning,
+    module='peewee'
+)
+
+
+def generate_id() -> str:
+    uuid5 = uuid.uuid4()
+    return str(uuid5)
+
+
+class Chat(BaseModel):
+    class Meta:
+        db_table = 'chats'
+        table_name = 'chats'
+
+    id = peewee.AutoField()
+    public_id = peewee.CharField(null=False, index=True, unique=True, default=generate_id)
+    course = peewee.ForeignKeyField(Course, null=False, backref='chats', on_delete='CASCADE')
+    session = peewee.ForeignKeyField(Session, null=False, backref='sessions', on_delete='CASCADE')
