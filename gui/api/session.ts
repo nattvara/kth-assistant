d41c8127a6a40154ee0cd5fbea669040021bb35a
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-17 16:11:35 +0100

    add session api calls using the fetch api

---
+++ gui/api/session.ts
@@ -0,0 +1,44 @@
+import Cookies from "js-cookie";
+import { HttpError, makeUrl } from "./http";
+
+export interface Session {
+  public_id: string;
+  message: string;
+}
+
+export async function startSession(): Promise<Session> {
+  const response = await fetch(makeUrl(`/session`), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const errorBody = await response.json();
+    throw new HttpError(response, errorBody);
+  }
+
+  const data = await response.json() as Session;
+  return data;
+}
+
+export async function getSession(): Promise<Session> {
+  const sessionCookie = Cookies.get("session_id") as string;
+
+  const response = await fetch(makeUrl(`/session/me`), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Session-ID": sessionCookie,
+    },
+  });
+
+  if (!response.ok) {
+    const errorBody = await response.json();
+    throw new HttpError(response, errorBody);
+  }
+
+  const data = await response.json() as Session;
+  return data;
+}
