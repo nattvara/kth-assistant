07d8830dcc619645ddfbe31024a3e50b1e657caf
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-22 18:08:39 +0100

    add prompts and formatters for a chat conversation

---
+++ services/llm/prompts.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from services.llm.formatters import format_messages
+from services.chat.system import SYSTEM_NAME
+from db.models import Message
+
+
+def prepend_system_prompt(system_prompt: str, prompt: str) -> str:
+    return f"""
+Chat rules:
+===================================
+
+{system_prompt}
+
+{prompt}
+"""
+
+
+def prompt_make_next_ai_message(messages: List[Message]) -> str:
+    return f"""
+Chat history:
+{format_messages(messages)}
+
+Next message from {SYSTEM_NAME}:
+<{SYSTEM_NAME}>:
+"""
