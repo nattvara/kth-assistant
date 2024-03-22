80112a660ca145bf1aa4f3a473b58496c2f6c34f
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-22 18:43:32 +0100

    add check new message is sent from assistant

--- tests/services/chat/chat_service_test.py
+++ tests/services/chat/chat_service_test.py
@@ -11,3 +11,4 @@ def test_chat_service_can_produce_message_for_chat_without_any_index(mocker, new
     mock_prompt.assert_called_once()
     mock_prompt.assert_called_with(messages)
     assert len(new_chat.chat.messages) == len(messages) + 1
+    assert message.sender == message.Sender.ASSISTANT
