2797af28d721e1fe9d552c8d0b3277cea2234acf
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-22 22:08:30 +0100

    add an alert at the top of the message feed that shows the properties of a chat

--- gui/components/chat/index.ts
+++ gui/components/chat/index.ts
@@ -1,5 +1,6 @@
+import ChatProperties from "./ChatProperties/ChatProperties";
 import ChatWindow from "./ChatWindow/ChatWindow";
 import Message from "./Message/Message";
 import MessageFeed from "./MessageFeed/MessageFeed";
 
-export { Message, MessageFeed, ChatWindow };
+export { Message, MessageFeed, ChatWindow, ChatProperties };
