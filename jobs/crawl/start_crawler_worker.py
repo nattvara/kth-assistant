fa10b54883ce5bf85f26ddbbd2718f84ec5c6ab1
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-25 00:08:29 +0100

    wrap job logic in run_worker function that makes it clearer what's the worker logic

--- jobs/crawl/start_crawler_worker.py
+++ jobs/crawl/start_crawler_worker.py
@@ -17,24 +17,28 @@ def start_job_again_in(seconds: int):
     schedule_job_start_crawler_worker(seconds)
 
 
+async def run_worker():
+    async with playwright.async_api.async_playwright() as pl:
+        browser = await pl.chromium.launch(headless=True)
+        context = await browser.new_context(viewport={'width': 800, 'height': 1000})
+        page = await context.new_page()
+
+        redis = await cache.redis.get_redis_connection()
+        service = get_crawler_service(redis, browser, context, page)
+
+        while service.has_next():
+            try:
+                url = await service.checkout()
+                await service.crawl_url(url)
+            except LockAlreadyAcquiredException:
+                log().debug("Found a lock on the url. Skipping.")
+
+        await redis.aclose()
+
+
 async def job():
     try:
-        async with playwright.async_api.async_playwright() as pl:
-            browser = await pl.chromium.launch(headless=True)
-            context = await browser.new_context(viewport={'width': 800, 'height': 1000})
-            page = await context.new_page()
-
-            redis = await cache.redis.get_redis_connection()
-            service = get_crawler_service(redis, browser, context, page)
-
-            while service.has_next():
-                try:
-                    url = await service.checkout()
-                    await service.crawl_url(url)
-                except LockAlreadyAcquiredException:
-                    log().debug("Found a lock on the url. Skipping.")
-
-            await redis.aclose()
+        await run_worker()
     finally:
         log().info(f"start_crawler_worker complete. enqueuing next job in 60 seconds.")
         start_job_again_in(60)
