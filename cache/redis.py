31c37e5f261213dc13fde6f5dd238903a0069152
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-24 23:55:27 +0100

    add get_redis_connection_sync to redis connection options so that syncronous functions can use redis too

--- cache/redis.py
+++ cache/redis.py
@@ -1,13 +1,24 @@
 from redis.asyncio import Redis
+from redis import Redis as RedisSync
 
 import config.settings as settings
 
 
-async def get_redis_connection():
+async def get_redis_connection() -> Redis:
     redis = Redis(
         host=settings.get_settings().REDIS_HOST,
         port=settings.get_settings().REDIS_PORT,
         decode_responses=True
     )
     await redis.ping()
+    return redis
+
+
+def get_redis_connection_sync() -> RedisSync:
+    redis = RedisSync(
+        host=settings.get_settings().REDIS_HOST,
+        port=settings.get_settings().REDIS_PORT,
+        decode_responses=True
+    )
+    redis.ping()
     return redis
