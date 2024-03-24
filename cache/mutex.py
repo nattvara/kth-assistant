df47afb59d2ffdae1281a4d06e7f2a6cf653967c
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-24 13:58:22 +0100

    reduce lock lifetime to one second

--- cache/mutex.py
+++ cache/mutex.py
@@ -5,7 +5,7 @@ class LockAlreadyAcquiredException(Exception):
     pass
 
 
-async def acquire_lock(redis: Redis, key: str, expire: int = 1000000):
+async def acquire_lock(redis: Redis, key: str, expire: int = 1000):
     acquired = await redis.set(key, "locked", nx=True, px=expire)
     if not acquired:
         raise LockAlreadyAcquiredException("Lock is already acquired.")
