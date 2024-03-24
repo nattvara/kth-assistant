4e232bcdd945e23f7306032149724668453c570e
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-24 12:27:37 +0100

    fix issue where new chats could be started from inactive chat configs

--- db/actions/chat_config.py
+++ db/actions/chat_config.py
@@ -3,4 +3,4 @@ from peewee import fn
 
 def get_random_chat_config():
     from db.models.chat_config import ChatConfig
-    return ChatConfig.select().order_by(fn.Random()).first()
+    return ChatConfig.select().where(ChatConfig.is_active == True).order_by(fn.Random()).first()  # noqa
