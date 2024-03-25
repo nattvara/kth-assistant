ed84296c8810baea765e06318dcbf875dc9ed2dc
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-25 22:42:46 +0100

    add method to LLMService that allows the caller to wat for a handle to finish, so the caller doesn't have to stream the response back

--- services/llm/llm.py
+++ services/llm/llm.py
@@ -1,4 +1,6 @@
+import asyncio
 from typing import Optional
+import time
 
 from redis.asyncio import Redis
 
@@ -13,6 +15,10 @@ class NoPendingPromptHandleError(Exception):
     pass
 
 
+class LLMServiceTimeoutException(Exception):
+    pass
+
+
 class LLMService:
 
     def __init__(self, model: LLMModel, redis: Redis):
@@ -23,6 +29,18 @@ class LLMService:
     def dispatch_prompt(prompt: str, model_name: LLMModel, model_params: Optional[Params] = None) -> PromptHandle:
         handle = PromptHandle(prompt=prompt, model_name=model_name, model_params=model_params)
         handle.save()
+        return handle
+
+    @staticmethod
+    async def wait_for_handle(handle: PromptHandle, timeout_seconds: int = 120) -> PromptHandle:
+        start_time = time.monotonic()
+        while True:
+            handle.refresh()
+            if handle.state == PromptHandle.States.FINISHED:
+                break
+            elif time.monotonic() - start_time > timeout_seconds:
+                raise LLMServiceTimeoutException(f"Waiting for prompt handle {handle.id} timed out.")
+            await asyncio.sleep(0.01)
         return handle
 
     async def checkout(self) -> PromptHandle:
