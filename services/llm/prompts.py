ce1949e0df9c57046abfdc78b2f604714f8cda7e
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-25 22:39:40 +0100

    update chat service to add support for generating questions based on the chat history

--- services/llm/prompts.py
+++ services/llm/prompts.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from services.llm.formatters import format_messages
+from services.llm.formatters import format_messages, format_documents
+from services.chat.docs import PostProcessedDocument
 from services.chat.system import SYSTEM_NAME
 from db.models import Message
 
@@ -25,4 +26,22 @@ Chat history:
 The next message in the conversion:
 ===================================
 <{SYSTEM_NAME}>:
+"""
+
+
+def prompt_generate_question_from_chat_history(messages: List[Message]) -> str:
+    history = messages[:-1]
+    last = messages[-1]
+    return f"""
+You are a completion generator. You should only produce questions used to search in a vector database.
+Combine the chat history and follow up question into a standalone question. If the follow up question isn't a question,
+respond with the exact string "NO_QUESTION".
+
+Chat History:
+{format_messages(history)}
+
+Follow up question:
+{last.content}
+
+Standalone question:
 """
