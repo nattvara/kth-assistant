d7f31aae57dccf46680880a36dd3ef90b2db2d05
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-03-25 22:41:53 +0100

    add formatters for post processed docs

--- services/llm/formatters.py
+++ services/llm/formatters.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from services.chat.docs import PostProcessedDocument
 from services.chat.system import SYSTEM_NAME
 from db.models import Message
 
@@ -19,3 +20,20 @@ def format_message(message: Message) -> str:
         sender = SYSTEM_NAME
         content = message.prompt_handle.response
     return f"<{sender}>: {content}"
+
+
+def format_documents(docs: List[PostProcessedDocument]) -> str:
+    out = ""
+    for doc in docs:
+        out += format_document(doc) + "\n"
+    return out
+
+
+def format_document(doc: PostProcessedDocument) -> str:
+    return f"""
+name: {doc.name}
+url: {doc.url}
+```
+{doc.text}
+```
+"""
