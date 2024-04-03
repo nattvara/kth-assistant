2c23f1e5016b24305b783366cebe5d893ab6abc0
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-03 17:44:24 +0200

    add a slightly better index page

--- gui/pages/index.tsx
+++ gui/pages/index.tsx
@@ -1,8 +1,18 @@
+import { Blockquote, Grid } from "@mantine/core";
+import { IconInfoCircle } from "@tabler/icons-react";
 import { GetServerSidePropsContext } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const IndexPage = () => {
-  return <p>foo</p>;
+  return (
+    <Grid>
+      <Grid.Col span={10} offset={1}>
+        <Blockquote color="blue" cite="Canvas Copilot" icon={<IconInfoCircle />} mt="xl">
+          No course selected.
+        </Blockquote>
+      </Grid.Col>
+    </Grid>
+  );
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
