c49d8a1f59fcc6507bbb6c9134c17ceb2fa841e6
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-04 21:09:37 +0200

    update indexing service to compute 3 vector embeddings for each url and include them with the indexed document

--- services/index/index.py
+++ services/index/index.py
@@ -1,8 +1,10 @@
 from typing import List
 
 from services.index.chunks import split_text_with_overlap
+from services.llm.supported_models import LLMModel
 import services.index.opensearch as search
 from db.models import Url, Snapshot
+import services.llm.llm as llm
 
 
 class IndexService:
@@ -10,21 +12,43 @@ class IndexService:
     def __init__(self):
         self.client = search.get_client()
 
-    def index_url(self, url: Url):
+    async def index_url(self, url: Url):
         if not search.index_exists(self.client, url.snapshot.id):
             search.create_index(self.client, url.snapshot.id)
 
         chunks = split_text_with_overlap(url.content.text)
         for idx, chunk in enumerate(chunks):
+            intfloat_me5li = await self._get_intfloat_me5li_embeddings(chunk)
+            sfr_embedding_mistral = await self._get_sfr_embedding_mistral_embeddings(chunk)
+            text_embedding_3_large = await self._get_text_embedding_3_large_embeddings(chunk)
+
             search.index_document(self.client, url.snapshot, f"{url.id}-{idx}", {
                 'name': url.content.name,
                 'text': chunk,
                 'url': url.href,
+                'sfr_embedding_mistral': sfr_embedding_mistral,
+                'intfloat_me5li': intfloat_me5li,
+                'text_embedding_3_large': text_embedding_3_large,
             })
 
         url.refresh()
         url.state = Url.States.INDEXED
         url.save()
+
+    async def _get_sfr_embedding_mistral_embeddings(self, text: str) -> List[float]:
+        handle = llm.LLMService.dispatch_prompt(text, LLMModel.SALESFORCE_SFR_EMBEDDING_MISTRAL)
+        handle = await llm.LLMService.wait_for_handle(handle)
+        return handle.embedding
+
+    async def _get_intfloat_me5li_embeddings(self, text: str) -> List[float]:
+        handle = llm.LLMService.dispatch_prompt(text, LLMModel.INTFLOAT_MULTILINGUAL_E5_LARGE_INSTRUCT)
+        handle = await llm.LLMService.wait_for_handle(handle)
+        return handle.embedding
+
+    async def _get_text_embedding_3_large_embeddings(self, text: str) -> List[float]:
+        handle = llm.LLMService.dispatch_prompt(text, LLMModel.OPENAI_TEXT_EMBEDDING_3_LARGE)
+        handle = await llm.LLMService.wait_for_handle(handle)
+        return handle.embedding
 
     def query_index(self, snapshot: Snapshot, query: str) -> List[search.Document]:
         return search.search_index(self.client, snapshot.id, query)
--- tests/services/index/index_test.py
+++ tests/services/index/index_test.py
@@ -1,51 +1,73 @@
+from unittest.mock import AsyncMock
+
+import pytest
+
+from services.llm.supported_models import LLMModel, EMBEDDING_MODELS
 from services.index.index import IndexService
-from db.models import Url
+from db.models import Url, PromptHandle
 
 
-def test_service_checks_if_index_exist_when_adding_url(new_snapshot, mocker):
+@pytest.mark.asyncio
+async def test_service_checks_if_index_exist_when_adding_url(new_snapshot, mocker):
     mocker.patch('services.index.opensearch.get_client')
     mock_index_exists = mocker.patch('services.index.opensearch.index_exists', return_value=True)
+    mocker.patch('services.llm.llm.LLMService.wait_for_handle')
 
     url = new_snapshot.add_url_with_content()
 
     index_service = IndexService()
 
-    index_service.index_url(url)
+    await index_service.index_url(url)
 
     mock_index_exists.assert_called_once()
 
 
-def test_service_creates_index_if_not_exists(new_snapshot, mocker):
+@pytest.mark.asyncio
+async def test_service_creates_index_if_not_exists(new_snapshot, mocker):
     mocker.patch('services.index.opensearch.get_client')
     mock_index_exists = mocker.patch('services.index.opensearch.index_exists', return_value=False)
     mock_create_index = mocker.patch('services.index.opensearch.create_index')
+    mocker.patch('services.llm.llm.LLMService.wait_for_handle')
 
     url = new_snapshot.add_url_with_content()
 
     index_service = IndexService()
 
-    index_service.index_url(url)
+    await index_service.index_url(url)
 
     mock_index_exists.assert_called_once()
     mock_create_index.assert_called_once()
 
 
-def test_service_can_index_url(new_snapshot, mocker):
+@pytest.mark.asyncio
+async def test_service_can_index_url(new_snapshot, mocker):
+    handle = PromptHandle(
+        prompt="a very long doc",
+        llm_model_name=LLMModel.SALESFORCE_SFR_EMBEDDING_MISTRAL,
+        embedding=[0.1, 0.2, 0.3]
+    )
+    handle.save()
+
     mocker.patch('services.index.opensearch.get_client', return_value=None)
     mocker.patch('services.index.opensearch.index_exists', return_value=False)
     mocker.patch('services.index.opensearch.create_index')
+    mocker.patch('services.llm.llm.LLMService.wait_for_handle', AsyncMock(return_value=handle))
+
     mock_index_document = mocker.patch('services.index.opensearch.index_document')
 
     url = new_snapshot.add_url_with_content()
 
     index_service = IndexService()
 
-    index_service.index_url(url)
+    await index_service.index_url(url)
 
     mock_index_document.assert_called_once_with(index_service.client, url.snapshot, f"{url.id}-0", {
         'name': url.content.name,
         'text': url.content.text,
         'url': url.href,
+        EMBEDDING_MODELS[LLMModel.OPENAI_TEXT_EMBEDDING_3_LARGE]: [0.1, 0.2, 0.3],
+        EMBEDDING_MODELS[LLMModel.SALESFORCE_SFR_EMBEDDING_MISTRAL]: [0.1, 0.2, 0.3],
+        EMBEDDING_MODELS[LLMModel.INTFLOAT_MULTILINGUAL_E5_LARGE_INSTRUCT]: [0.1, 0.2, 0.3],
     })
 
     url.refresh()
