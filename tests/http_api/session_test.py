c53a539076710f3a9c4d52d16497e17e276a8197
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-13 23:54:11 +0200

    add support to session api to grant consent for study

--- tests/http_api/session_test.py
+++ tests/http_api/session_test.py
@@ -27,3 +27,30 @@ def test_protected_route_with_invalid_session(api_client):
     response = api_client.get('/session/me', headers=headers)
 
     assert response.status_code == 401
+
+
+def test_consent_is_not_given_by_default(api_client):
+    response = api_client.post('/session')
+    data = response.json()
+
+    session = Session.select().filter(Session.public_id == data['public_id']).first()
+    assert session.consent is False
+
+
+def test_consent_can_be_given(api_client):
+    response = api_client.post('/session')
+    session = Session.select().filter(Session.public_id == response.json()['public_id']).first()
+
+    headers = {'X-Session-ID': response.json()['public_id'], 'Content-Type': 'application/json'}
+    response = api_client.post('/session/consent', json={'granted': True}, headers=headers)
+
+    session.refresh()
+    assert response.status_code == 200
+    assert session.consent is True
+
+
+def test_consent_can_be_checked(api_client, authenticated_session):
+    response = api_client.get('/session/me', headers=authenticated_session.headers)
+    data = response.json()
+
+    assert data['consent'] is True
