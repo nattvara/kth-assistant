44d1afeddada338d86159f7d7de8022d1e739cb8
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-14 23:28:28 +0200

    add Feedback component that renders feedback questions and allows the user to submit responses

---
+++ gui/components/chat/Feedback/Feedback.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { Message } from "@/api/chat";
+import { fetchFeedbackQuestions } from "@/api/feedback";
+
+import { SelectFeedback } from "./SelectFeedback";
+
+export interface FeedbackProps {
+  message: Message;
+  withLabel: boolean;
+  withPostLabel: boolean;
+  courseId: string;
+  chatId: string;
+}
+
+export default function Feedback(props: FeedbackProps) {
+  const { message, withLabel, withPostLabel, courseId, chatId } = props;
+  const questionQuery = useQuery({
+    queryKey: ["feedback_questions", message.feedback_id],
+    queryFn: () => fetchFeedbackQuestions(message.feedback_id as string),
+  });
+
+  if (questionQuery.isError) {
+    return <span>Failed to fetch feedback question: {questionQuery.error.message}</span>;
+  }
+
+  if (questionQuery.data === null || questionQuery.data === undefined) {
+    return <></>;
+  }
+
+  if (questionQuery.data.extra_data.type === "select") {
+    return (
+      <SelectFeedback
+        message={message}
+        feedback={questionQuery.data}
+        withLabel={withLabel}
+        withPostLabel={withPostLabel}
+        courseId={courseId}
+        chatId={chatId}
+      />
+    );
+  }
+
+  return <>Error: Unknown feedback type.</>;
+}
--- gui/components/chat/index.ts
+++ gui/components/chat/index.ts
@@ -1,8 +1,8 @@
 import ChatProperties from "./ChatProperties/ChatProperties";
 import ChatWindow from "./ChatWindow/ChatWindow";
 import Faqs from "./Faqs/Faqs";
-import FeedbackQuestions from "./FeedbackQuestions/FeedbackQuestions";
+import Feedback from "./Feedback/Feedback";
 import Message from "./Message/Message";
 import MessageFeed from "./MessageFeed/MessageFeed";
 
-export { Message, MessageFeed, ChatWindow, ChatProperties, Faqs, FeedbackQuestions };
+export { Message, MessageFeed, ChatWindow, ChatProperties, Faqs, Feedback };
