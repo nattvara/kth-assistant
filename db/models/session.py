94a34416c16cb03256b3aab206f02676858d7411
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-14 17:29:26 +0200

    add default_llm_model_name and default_index_type fields to Session model

--- db/models/session.py
+++ db/models/session.py
@@ -3,6 +3,7 @@ import uuid
 
 import peewee
 
+from db.custom_fields import ModelNameField, IndexTypeField
 from . import BaseModel
 
 # Suppress specific DeprecationWarning about db_table, this is needed for migrations to work
@@ -28,3 +29,5 @@ class Session(BaseModel):
     is_valid = peewee.BooleanField(default=True)
     public_id = peewee.CharField(null=False, index=True, unique=True, default=generate_id)
     consent = peewee.BooleanField(default=False)
+    default_llm_model_name = ModelNameField(null=False, index=True)
+    default_index_type = IndexTypeField(null=False, index=True)
