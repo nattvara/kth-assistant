43db3dc093838a17b883898edba8d06c44d4e5e1
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-14 23:25:15 +0200

    update the feedback api in the gui to reflect the simpler nature of the feedback api

--- gui/api/feedback.ts
+++ gui/api/feedback.ts
@@ -2,29 +2,21 @@ import Cookies from "js-cookie";
 
 import { HttpError, makeUrl } from "@/api/http";
 
-export interface Feedback {
-  language: string;
-  feedback_question_id: string;
-  message_id: string;
-  answer: string;
-}
+export const UNANSWERED = "UNANSWERED";
 
-export interface FeedbackQuestion {
+export interface Feedback {
   feedback_question_id: string;
   question: string;
   extra_data: {
-    choices: undefined | string[];
+    type: string;
+    choices: string[];
   };
 }
 
-export interface FeedbackQuestions {
-  questions: FeedbackQuestion[];
-}
-
-export async function fetchFeedbackQuestions(language: string): Promise<FeedbackQuestions> {
+export async function fetchFeedbackQuestions(feedbackId: string): Promise<Feedback> {
   const sessionCookie = Cookies.get("session_id") as string;
 
-  const response = await fetch(makeUrl(`/feedback/${language}`), {
+  const response = await fetch(makeUrl(`/feedback/${feedbackId}`), {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -37,19 +29,14 @@ export async function fetchFeedbackQuestions(language: string): Promise<Feedback
     throw new HttpError(response, errorBody, response.status);
   }
 
-  const data = (await response.json()) as FeedbackQuestions;
+  const data = (await response.json()) as Feedback;
   return data;
 }
 
-export async function sendFeedback(
-  language: string,
-  feedbackQuestionId: string,
-  messageId: string,
-  choice: string,
-): Promise<Feedback> {
+export async function sendFeedback(feedback_id: string, choice: string): Promise<Feedback> {
   const sessionCookie = Cookies.get("session_id") as string;
 
-  const response = await fetch(makeUrl(`/feedback/${language}/questions/${feedbackQuestionId}/messages/${messageId}`), {
+  const response = await fetch(makeUrl(`/feedback/${feedback_id}`), {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -67,53 +54,4 @@ export async function sendFeedback(
 
   const data = (await response.json()) as Feedback;
   return data;
-}
-
-export async function fetchFeedback(
-  language: string,
-  feedbackQuestionId: string,
-  messageId: string,
-): Promise<Feedback> {
-  const sessionCookie = Cookies.get("session_id") as string;
-
-  const response = await fetch(makeUrl(`/feedback/${language}/questions/${feedbackQuestionId}/messages/${messageId}`), {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Session-ID": sessionCookie,
-    },
-  });
-
-  if (!response.ok) {
-    const errorBody = await response.json();
-    throw new HttpError(response, errorBody, response.status);
-  }
-
-  const data = (await response.json()) as Feedback;
-  return data;
-}
-
-export async function fetchFeedbackIsAnswered(
-  language: string,
-  feedbackQuestionId: string,
-  messageId: string,
-): Promise<boolean> {
-  const sessionCookie = Cookies.get("session_id") as string;
-
-  const response = await fetch(makeUrl(`/feedback/${language}/questions/${feedbackQuestionId}/messages/${messageId}`), {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Session-ID": sessionCookie,
-    },
-  });
-
-  if (!response.ok) {
-    if (response.status === 404) return false;
-
-    const errorBody = await response.json();
-    throw new HttpError(response, errorBody, response.status);
-  }
-
-  return true;
 }
