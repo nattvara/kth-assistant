99d7693895fba196130f277d30599425c5ed6128
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-15 22:18:57 +0200

    add matomo tracker script

--- gui/pages/_document.tsx
+++ gui/pages/_document.tsx
@@ -6,6 +6,21 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <ColorSchemeScript />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//matomo.kthgpt.com/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `
+        }} />
       </Head>
       <body>
         <Main />
