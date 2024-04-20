5413d43099189bb1161969e9f76726f8754945ed
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-20 14:09:26 +0200

    add link to kth e-learning website

--- gui/components/promo/Hero/Hero.tsx
+++ gui/components/promo/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Space, Text, Title } from "@mantine/core";
+import { Anchor, Button, Container, Space, Text, Title } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { IconArrowDown } from "@tabler/icons-react";
 import { useRouter } from "next/router";
@@ -18,12 +18,15 @@ const Hero = () => {
               <span className={styles.primary_color}>Canvas AI Copilot</span> - an AI Powered Assistant in Canvas.
             </Title>
           </div>
-
           <div className={styles.description_text}>
             <Text size="xl">
               Canvas AI Copilot is a smart assistant integrated into Canvas course rooms, designed to enhance the
               E-learning experience by creating a favorable learning environment. This tool is part of a master thesis
-              at KTH IT exploring how chatbots can facilitate E-learning, conducted during
+              at<span> </span>
+              <Anchor href="https://intra.kth.se/utbildning/e-larande" target="_blank">
+                KTH&apos;s E-Learning unit
+              </Anchor>
+              , exploring how chatbots can facilitate E-learning, conducted during
               <strong> the spring term of 2024</strong>.
             </Text>
           </div>
