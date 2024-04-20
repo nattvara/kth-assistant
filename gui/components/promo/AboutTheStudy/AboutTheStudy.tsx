b5903f2f8e7dfd735a65ecaa5efbeff3a7b7103c
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-04-20 14:01:20 +0200

    update copy in about the study

--- gui/components/promo/AboutTheStudy/AboutTheStudy.tsx
+++ gui/components/promo/AboutTheStudy/AboutTheStudy.tsx
@@ -11,20 +11,18 @@ const AboutTheStudy = () => {
             About the study
           </Title>
           <Text className={styles.text}>
-            The study evaluates different tools and techniques for developing AI assistants, focusing specifically on
-            the use of Large Language Models (LLMs) and Retrieval Augmented Generation (RAG) techniques, such as
-            traditional search algorithms like TF/IDF and advanced vector embeddings. The goal is to ascertain which
-            methods are most effective at accurately sourcing and managing course-specific information within a learning
-            management system. This part of the study will help pinpoint the most efficient AI models and retrieval
-            techniques to enhance educational experiences by facilitating better access to content.
+            The study evaluates various tools and techniques for developing AI assistants, with a special focus on the
+            use of Large Language Models (LLMs) and Retrieval Augmented Generation (RAG) techniques like TF/IDF and
+            advanced vector embeddings. The aim is to determine the most effective methods for accurately sourcing and
+            managing course-specific information within a learning management system.
           </Text>
 
           <Text className={styles.text}>
-            Additionally, the study assesses student preferences for the functionality and effectiveness of these AI
-            assistants. Through targeted surveys, it will gather both qualitative and quantitative feedback concerning
-            the accuracy of information retrieval, the responsiveness of the AI systems, and overall user satisfaction.
-            The results will provide crucial insights into which AI configurations are most aligned with user needs,
-            guiding further development of AI applications in educational environments.
+            The study also evaluates student preferences for the functionality and effectiveness of these AI assistants
+            through questions in the chat, collecting both qualitative and quantitative feedback on information
+            retrieval accuracy, AI system responsiveness, and overall student satisfaction. The findings will provide
+            vital insights into the AI configurations that best align with user needs, guiding further development of AI
+            applications in educational settings.
           </Text>
         </SimpleGrid>
       </Container>
