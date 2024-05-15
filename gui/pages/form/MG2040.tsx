20daac0b2c4e45bc4ceae51b9f3193c5ebc3aca0
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-05-15 22:46:23 +0200

    add quick forward to typeform form at the link /form/MG2040

---
+++ gui/pages/form/MG2040.tsx
@@ -0,0 +1,14 @@
+const FormPage = () => {
+  return null;
+};
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "https://rcuf98viy48.typeform.com/to/DdfM15f2",
+      permanent: false,
+    },
+  };
+}
+
+export default FormPage;
