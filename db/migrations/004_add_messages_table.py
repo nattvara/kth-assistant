720dfe1faac5583c5524a569506ebb0da22331cd
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-06-12 19:14:15 +0200

    fix broken broken migration, message depends upon the Faq and FaqSnapshot models

--- db/migrations/004_add_messages_table.py
+++ db/migrations/004_add_messages_table.py
@@ -1,13 +1,13 @@
 from peewee_migrate import Migrator
 import peewee as pw
 
-from db.models import Message
+from db.models import Message, Faq, FaqSnapshot
 from db.connection import db
 
 
 def migrate(migrator: Migrator, database: pw.Database, fake=False, **kwargs):
-    db.create_tables([Message])
+    db.create_tables([Message, Faq, FaqSnapshot])
 
 
 def rollback(migrator: Migrator, database: pw.Database, fake=False, **kwargs):
-    db.drop_tables([Message])
+    db.drop_tables([Message, Faq, FaqSnapshot])
